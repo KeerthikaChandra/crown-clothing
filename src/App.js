d41e11deb24da1bd9fc65b6de71a77a028249df6
KeerthikaChandra <keerthikachandrashekhar0410@gmail.com>
2019-12-04 01:09:40 +0530

    updated menu-item component withRouter HOC for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './page/homepage/homepage.component';
 
+const HatPage = (props) => {
+  console.log(props)
+ return (<div>
+    <button onClick={() => props.history.push('/')}>Home Page</button>
+    <h1>Hat Page</h1> 
+  </div>)
+}
+ 
+  
+
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/hats' component={HatPage} />
+      </Switch> 
+    </div>
   );
 }
 
