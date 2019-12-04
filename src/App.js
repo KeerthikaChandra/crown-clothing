521a5fde056eedb2b90413acb6bb3760dda8eebf
KeerthikaChandra <keerthikachandrashekhar0410@gmail.com>
2019-12-04 23:24:43 +0530

    created a ShopPage with the components collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './page/homepage/homepage.component';
-
-const HatPage = (props) => {
-  console.log(props)
- return (<div>
-    <button onClick={() => props.history.push('/')}>Home Page</button>
-    <h1>Hat Page</h1> 
-  </div>)
-}
- 
-  
+import ShopPage from './page/shop/shop.component';
 
 
 function App() {
@@ -20,7 +11,7 @@ function App() {
     <div>
       <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={HatPage} />
+      <Route path='/shop' component={ShopPage} />
       </Switch> 
     </div>
   );
