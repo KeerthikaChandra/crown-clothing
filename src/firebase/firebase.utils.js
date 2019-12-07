738efa6704c599d5f13661c923eff25f2955da08
KeerthikaChandra <keerthikachandrashekhar0410@gmail.com>
2019-12-08 01:15:49 +0530

    added redux and added userReducer and user.action and modified app and header components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,62 +1,50 @@
 import firebase from 'firebase/app';
-
 import 'firebase/firestore';
-
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCtCzMnAI82pJTHv0w3AOTaBDZG4OQZHg0",
-    authDomain: "crown-db-6bc6b.firebaseapp.com",
-    databaseURL: "https://crown-db-6bc6b.firebaseio.com",
-    projectId: "crown-db-6bc6b",
-    storageBucket: "crown-db-6bc6b.appspot.com",
-    messagingSenderId: "633879615441",
-    appId: "1:633879615441:web:bd9627d381ad177e9af2f0",
-    measurementId: "G-7ZZ1BE319E"
-  };
+apiKey: "AIzaSyDl94jos3mRxvT_1Mr6jlf1EKlhk37lSp0",
+authDomain: "crwn-db-f4d7e.firebaseapp.com",
+databaseURL: "https://crwn-db-f4d7e.firebaseio.com",
+projectId: "crwn-db-f4d7e",
+storageBucket: "crwn-db-f4d7e.appspot.com",
+messagingSenderId: "86030205219",
+appId: "1:86030205219:web:c53a651aabc307ab4d8c96",
+measurementId: "G-0RGQYJ0HQL"
+};
+
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
+  if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
-  const snapShot = await userRef.get();
- 
-  if(!snapShot.exists)  {
-    const {displayName,email} = userAuth
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get(); 
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
-        displayName, 
+        displayName,
         email,
         createdAt,
         ...additionalData
-      })
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
-
-    catch(error) {
-      console.log('error creating user',error.message);
-    }
-    
   }
+
   return userRef;
+};
 
-} 
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  firebase.initializeApp(config);
-
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
-
-  const provider = new firebase.auth.GoogleAuthProvider();
-
-  provider.setCustomParameters({prompt: 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-//   export const signInWithGoogle = () =>  
-//       auth.signInWithPopup(provider)
-//       .then(function (result) { var token = result.credential.accessToken; console.log(token)})
-//       .catch(function (error) { console.log("--------sign in with popup error ----", error.message); });
-    
-
-  export default firebase;
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import store from '../src/redux/store';
 
 
 
 ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
     , document.getElementById('root'));
 
 
